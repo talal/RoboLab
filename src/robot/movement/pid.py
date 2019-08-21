b2a8ec5adb92daafff23253f348d3f2aa71e543d
Muhammad Talal Anwar <3526562+talal@users.noreply.github.com>
2019-08-21 15:58:23 +0200

    improve PID controller's calibration

--- src/robot/movement/pid.py
+++ src/robot/movement/pid.py
@@ -26,32 +26,27 @@ class PIDController:
         cs = ColorSensor()
         color_intensity_list = list()
 
-        # rotate to 40 degrees using the given rotate_func at increments of 5 degrees
-        # while appending the light intensity to the collective list
-        def rotate_and_append(rotate_func):
-            for i in range(0, 40, 5):  # get light intensity
+        def rotate_and_append_light_intensity(rotate_func):
+            rotate_func(angle=30, speed=180)
+            while mc.are_running():
                 color_intensity_list.append(cs.get_reflected_color_intensity())
-                rotate_func(angle=5, speed=180)
-            rotate_func(angle=(-1 * 40), speed=180)  # go back to the starting position
+            rotate_func(angle=(-1 * 30), speed=180)  # go back to the starting position
+            mc.wait_while_running()
 
-        rotate_and_append(mc.rotate_clockwise)
-        rotate_and_append(mc.rotate_counter_clockwise)
-
-        min_intensity = min(color_intensity_list)
-        max_intensity = max(color_intensity_list)
-        # proportionality constant, i.e (change in y-axis)/(change in x-axis)
-        # where y-axis = motor power and x-axis = color intensity
-        proportionality_constant = (drive_speed - (-1 * drive_speed)) / (
-            max_intensity - min_intensity
-        )
-        proportionality_constant *= 0.65
+        rotate_and_append_light_intensity(mc.rotate_clockwise)
+        rotate_and_append_light_intensity(mc.rotate_counter_clockwise)
 
         # TODO: do test runs on the big planet and fine-tune if needed
-        self.offset = int((max_intensity - min_intensity) / 2)
-        self.tp = drive_speed  # target power at which both motors run when error = 0
-        self.kp = round(proportionality_constant, 3)
-        self.ki = round((0.1 * proportionality_constant), 3)
-        self.kd = round((0.44 * proportionality_constant), 3)
+        self.adjustment_constant = min(color_intensity_list)
+        min_intensity = min(color_intensity_list)
+        max_intensity = max(color_intensity_list)
+        critical_gain = (drive_speed - (-1 * drive_speed)) / (max_intensity - min_intensity)
+
+        self.offset = int(((max_intensity + min_intensity) * 0.5))
+        self.tp = drive_speed
+        self.kp = 0.2 * critical_gain
+        self.ki = 0.125 * critical_gain
+        self.kd = 0.3 * critical_gain
 
         logger.debug(
             f"calibrating PID controller using color intensity values = {color_intensity_list}"
@@ -67,11 +62,12 @@ class PIDController:
         # integral tends to accumulate drastically and causes overshooting if the
         # robot loses the line for too long therefore we do a pre-update correction
         values_have_opposite_signs = lambda x, y: ((x ^ y) < 0)
-        if error == 0 or values_have_opposite_signs(error, self.last_error):
+        if (-5 < error < 5) or values_have_opposite_signs(error, self.last_error):
             self.integral = 0
+            return 0
         else:
             # TODO: integral dampening is not finalized, it might require fine-tuning
-            self.integral = (0.1 * self.integral) + error
+            self.integral = ((2 / 3) * self.integral) + error
 
         # use the updated integral to calculate turn component
         return self.ki * self.integral
@@ -90,14 +86,9 @@ class PIDController:
         d = self.calculate_d_component(error)
 
         self.last_error = error  # this MUST be at the end, just before return
-        return p + i + d
+        return round((p + i + d), 3)
 
-    def reset(self):
-        self.offset = 0
-        self.tp = 0
-        self.kp = 0
-        self.ki = 0
-        self.kd = 0
+    def reset_id_components(self):
         self.integral = 0
         self.last_error = 0
-        logger.debug("PID controller has been reset")
+        logger.debug("PID controller's integral and last error values have been reset")
